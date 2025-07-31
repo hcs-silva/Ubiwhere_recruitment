611637a4617fb00123122c4d0cbd8c19b526f852
Hernâni Silva <hcsilvamatos@hotmail.com>
2025-07-31 22:26:10 +0100

    feat: Started marker logic and fetching of earthquake data
    feat: add AuthConfig to avoid repetition on http requests.
    
    backlog: correct need to be logged in message

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,22 +1,31 @@
 import L from "leaflet";
-import { useEffect } from "react";
-import style from "../styles/Map.module.css"
+import { useContext, useEffect, useState } from "react";
+import style from "../styles/Map.module.css";
+import { AuthContext } from "../contexts/AuthContext.tsx";
+import axios from "axios";
 
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
 
 function Map() {
-    type LeafletMapElement = HTMLElement & { _leaflet_id?: number };
+
+    const [earthquakes, setEarthquakes] = useState([]);
+  type LeafletMapElement = HTMLElement & { _leaflet_id?: number };
+
+  const authContext = useContext(AuthContext);
+  if (!authContext) {
+    throw new Error("AuthContext must be used within an AuthWrapper");
+  }
+
+  const { getAuthConfig } = authContext;
 
   useEffect(() => {
-
-    //check if the map container exists,and if it does do not create another instance
+    //Check if the map container exists,and if it does do not create another instance
 
     const mapElement = L.DomUtil.get("map") as LeafletMapElement;
     if (mapElement?._leaflet_id) {
-        mapElement._leaflet_id = undefined; // or null
+      mapElement._leaflet_id = undefined; // or null
     }
-    // if((L.DomUtil.get("map") as any)?._leaflet_id){
-    //     (L.DomUtil.get("map") as any)._leaflet_id = null; // reset the id to allow reinitialization
-    // }
+    // Initialize the map
     const map = L.map("map").setView([40.64427, -8.64554], 13);
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -25,12 +34,32 @@ function Map() {
       maxZoom: 19,
     }).addTo(map);
 
-    //map cleanup on unmount
+    //Add markers
+
+    //Map cleanup on unmount
     return () => {
       map.remove();
-    }
+    };
   }, []);
 
+  const getEarthquakes = async () => {
+    try {
+      await axios.get(`${BACKEND_URL}/earthquakes`, getAuthConfig()).then((response) => {
+      setEarthquakes(response.data);})
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  //TODO: add one marker to the map for testing purposes and then loop through the earthquakes array to add markers for each earthquake
+  //TODO: Complete reading the documentation on the leaflet library to understand how to add markers and popups
+
+
+  earthquakes.map((earthquake: any) => {
+    const { coordiantes } = earthquake;
+    console.log("Coordinates:", coordiantes);
+    // const marker = L.marker([coordiantes.lat, coordiantes.long]).addTo(map);    
+  })
+  
   return <div id="map" className={style.map}></div>;
 }
 
--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,12 +1,15 @@
 import { useNavigate } from "react-router-dom";
-import { useState, createContext } from "react";
-// import axios from "axios";
+import { useState, createContext, useEffect } from "react";
+import type { AxiosRequestConfig } from "axios";
 
+
+ // Replace with your backend URL
 type AuthContextType = {
   isLoggedIn: boolean;
   setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
   handleLogout: () => void;
-  authenticateUser: (token: string) => void;
+  authenticateUser: (token: string) => void;  
+  getAuthConfig: () => AxiosRequestConfig;
 };
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -17,11 +20,30 @@ type AuthWrapperProps = {
 const AuthWrapper: React.FC<AuthWrapperProps> = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+const getAuthConfig = () => ({
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+      'Content-Type': 'application/json',
+    }
+  });
+
+  //Check if token exists in localStorage on initial load(to ensute persistence os the authentication state)
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token && token !== "null") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const authenticateUser = (token: string) => {
     if (!token || token === "null") {
       setIsLoggedIn(false);
+    } else {
+      setIsLoggedIn(true);
     }
   };
+
+
 
   const navigate = useNavigate();
 
@@ -33,7 +55,7 @@ const AuthWrapper: React.FC<AuthWrapperProps> = ({ children }) => {
 
   return (
     <AuthContext.Provider
-      value={{ isLoggedIn, setIsLoggedIn, handleLogout, authenticateUser }}
+      value={{ isLoggedIn, setIsLoggedIn, handleLogout, authenticateUser , getAuthConfig}}
     >
       {children}
     </AuthContext.Provider>
